9cf9d56c58e51c8cc0de356d7ddee60e071c40e2
Stiven Castaño <jhojan.castano@correounivalle.edu.co>
2023-12-17 21:11:21 -0500

    correccion y nueva paralelizacion

--- app/src/main/scala/taller4/DNA.scala
+++ app/src/main/scala/taller4/DNA.scala
@@ -84,7 +84,6 @@ class DNA {
         (combinaciones._1.join() ++ combinaciones._2.join()).toList
       }
     }
-
     val s = test(n).to(LazyList).filter(o).head
     s
   }
@@ -111,35 +110,42 @@ class DNA {
         }
       }
     }
-
     GenerarCadenaMejorada(1, Seq(Seq.empty[Char]))
   }
 
-  def reconstruirCadenaMejoradoPar(n: Int, o: Oraculo): Seq[Char] ={
+
+  def reconstruirCadenaMejoradoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
     @tailrec
-    def GenerarCadenaMejorada(k: Int, SC: Seq[Seq[Char]]): Seq[Char] ={
+    def GenerarCadenaMejoradapar(k: Int, SC: Seq[Seq[Char]]): Seq[Char] = {
       if (k > n) Seq.empty[Char]
       else {
-        val (alfabeto1, alfabeto2) = alfabeto.splitAt(alfabeto.length / 2)
-        val newSecuenceCaracter1 = task{
-          SC.flatMap(seq => alfabeto1.map(c => seq :+ c)).filter(o)
-        }
-        val newSecuenceCaracter2 = task {
-          SC.flatMap(seq => alfabeto2.map(c => seq :+ c)).filter(o)
-        }
-        val newSecuenceCaracter = parallel(newSecuenceCaracter1, newSecuenceCaracter2)
-
-        newSecuenceCaracter._1.join().find(_.length == n) match {
-          case Some(resultado) => resultado
-          case None => newSecuenceCaracter._2.join().find(_.length == n) match {
+        if (k <= umbral) {
+          val newSC = SC.flatMap(seq => alfabeto.map(c => seq :+ c)).filter(o)
+          newSC.find(_.length == n) match {
             case Some(resultado) => resultado
-            case None => GenerarCadenaMejorada(k + 1, newSecuenceCaracter._1.join() ++ newSecuenceCaracter._2.join())
+            case None => GenerarCadenaMejoradapar(k + 1, newSC)
+          }
+        } else {
+          val (alfabeto1, alfabeto2) = alfabeto.splitAt(alfabeto.length / 2)
+          val newSecuenceCaracter1 = task {
+            SC.flatMap(seq => alfabeto1.map(c => seq :+ c)).filter(o)
+          }
+          val newSecuenceCaracter2 = task {
+            SC.flatMap(seq => alfabeto2.map(c => seq :+ c)).filter(o)
+          }
+          val newSecuenceCaracter = parallel(newSecuenceCaracter1, newSecuenceCaracter2)
+
+          newSecuenceCaracter._1.join().find(_.length == n) match {
+            case Some(resultado) => resultado
+            case None => newSecuenceCaracter._2.join().find(_.length == n) match {
+              case Some(resultado) => resultado
+              case None => GenerarCadenaMejoradapar(k + 1, newSecuenceCaracter._1.join() ++ newSecuenceCaracter._2.join())
+            }
           }
         }
       }
     }
-
-    GenerarCadenaMejorada(1, Seq(Seq.empty[Char]))
+    GenerarCadenaMejoradapar(1, Seq(Seq.empty[Char]))
   }
 
   /**
@@ -172,6 +178,35 @@ class DNA {
     }
   }
 
+  def reconstruirCadenaTurbopar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
+    if (n == 1) {
+      alfabeto.map(Seq(_)).find(o).getOrElse(Seq.empty)
+    } else {
+      def generarCadenaTurboPar(k: Int, SC: Seq[Seq[Char]]): Seq[Char] = {
+        if (k <= umbral) {
+          val newSC = SC.flatMap(seq => alfabeto.map(c => seq :+ c)).filter(o)
+          newSC.find(_.length == n).getOrElse(generarCadenaTurboPar(k + 1, newSC))
+        } else {
+          val (alfabeto1, alfabeto2) = alfabeto.splitAt(alfabeto.length / 2)
+          val nuevaSecuencia1 = task {
+            SC.flatMap(seq => alfabeto1.map(c => seq :+ c)).filter(o)
+          }
+          val nuevaSecuencia2 = task {
+            SC.flatMap(seq => alfabeto2.map(c => seq :+ c)).filter(o)
+          }
+          val NuevaSecuenciaCombi = parallel(nuevaSecuencia1, nuevaSecuencia2)
+          val resultado = NuevaSecuenciaCombi._1.join().find(_.length == n).orElse(NuevaSecuenciaCombi._2.join().find(_.length == n))
+
+          resultado.getOrElse(generarCadenaTurboPar(k + 1, NuevaSecuenciaCombi._1.join() ++ NuevaSecuenciaCombi._2.join()))
+        }
+      }
+
+      generarCadenaTurboPar(1, Seq(Seq.empty[Char]))
+    }
+  }
+
+
+
 
   def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
     if (n == 1) {
@@ -185,7 +220,7 @@ class DNA {
         if (resultado.nonEmpty) {
           resultado.head
         } else {
-          generarCadenaTurbo(k * 2, newSC) // probar velocidades multiplicando en vez de sumando (k+1) => (k*2)
+          generarCadenaTurbo(k + 1, newSC) // probar velocidades multiplicando en vez de sumando (k+1) => (k*2)
         }                                  //Considerar volver funcion fuera de la funcion para reutilizar arriba
       }
       def filtrar(SC: Seq[Seq[Char]], k: Int): Seq[Seq[Char]] = {
@@ -198,6 +233,8 @@ class DNA {
       generarCadenaTurbo(1, conjuntoInicial)
     }
   }
+
+
 }
 
 
--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -9,7 +9,7 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 import scala.annotation.tailrec
-
+import org.scalameter._
 
 
 
@@ -19,8 +19,9 @@ object Taller4 {
 
   def main(args: Array[String]): Unit = {
     val dna = new DNA()
-    val cadena = dna.randomDna(10)
+    val cadena = dna.randomDna(4)
     val or = Oraculo.crearOraculo(cadena)
+    val umbralcant = 4
 
     println("Cadena aleatoria:")
     println(cadena)
@@ -28,22 +29,46 @@ object Taller4 {
     //println("Resultado de reconstruirCadenaIngenuo:")
     //println(dna.reconstruirCadenaIngenuo(cadena.length,or))
 
+    //println("Resultado de ReconstruirCadenaMejorada: ")
+    //println(dna.reconstruirCadenaMejorado(cadena.length, or))
+
+    //println("Resultado de paralelizacion de Cadena Mejorada: ")
+    //println(dna.reconstruirCadenaMejoradoPar(4)(cadena.length, or))
+
+
     //println("Resultado de reconstruirCadenaTurbo:")
     //println(dna.reconstruirCadenaTurbo(cadena.length, or))
+
+    //println("Resultado de pralelizacon de Turbo: ")
+    //println(dna.reconstruirCadenaTurbopar(4)(cadena.length,or))
+
+
 
     //println("Resultado de reconstruirCadenaTurboMejorada:")
     //println(dna.reconstruirCadenaTurboMejorada(cadena.length, or))
 
-    //println("Resultado de ReconstruirCadenaMejorada: ")
-    //println(dna.reconstruirCadenaMejorado(cadena.length, or))
 
-    println("Resultado de paralelizacion de Cadena Mejorada: ")
-    println(dna.reconstruirCadenaMejoradoPar(cadena.length,or))
+    val timeMejo = withWarmer(new Warmer.Default) measure {
+      dna.reconstruirCadenaMejorado(cadena.length, or)
+    }
+    println(s"Time taken by mejorada: ${timeMejo.value * 1000} ms")
 
-    //val dna = new DNA()
-    //val duro = dna.reconstruirCadenaIngenuoPar(2,2)
-    //println(duro)
+    val timeMejopar = withWarmer(new Warmer.Default) measure {
+      dna.reconstruirCadenaMejoradoPar(umbralcant)(cadena.length, or)
+    }
+    println(s"Time taken by mejorada paralelizada: ${timeMejopar.value * 1000} ms")
+
+    val timeTurbo = withWarmer(new Warmer.Default) measure {
+      dna.reconstruirCadenaTurbo(cadena.length, or)
+    }
+    println(s"Time taken by turbo mejorada: ${timeTurbo.value * 1000} ms")
+
+    val timeTurbopar = withWarmer(new Warmer.Default) measure {
+      dna.reconstruirCadenaTurbopar(umbralcant)(cadena.length,or)
+    }
+    println(s"Time taken by turbo mejorada paralelizada: ${timeTurbopar.value * 1000} ms")
 
 
   }
+
 }
